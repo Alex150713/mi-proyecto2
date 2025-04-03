9bf65fcfc13a14791dd9c92635de0ef48147d123
Alex150713 <alexandrucristian2017@hotmail.com>
2025-04-03 23:56:23 +0200

    Añadida nueva funcionalidad:

--- Practicar.java
+++ Practicar.java
@@ -3,5 +3,6 @@ public class Practicar {
     public static void main(String[] args) {
         System.out.println("¡Holaaaaa, mundo!");
         System.out.println("Que tal?");
+        System.out.println("Repositorio de GITHUB");
     }
 }
